336b79d01f209e5778e88b37f1b21290ce36f5b0
Breno Rosado <brenorosado.cjr@gmail.com>
2026-05-10 23:39:16 -0300

    feat: add tests for projects module

--- apps/projects/tests.py
+++ apps/projects/tests.py
@@ -1,3 +1,142 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from apps.users.models import User
+from apps.workspaces.models import Workspace
+from apps.projects.models import Project
 
-# Create your tests here.
+
+class ProjectViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user('john@example.com', 'John', 'Xk9#mP2$qR7!')
+        self.other_user = User.objects.create_user('jane@example.com', 'Jane', 'Xk9#mP2$qR7!')
+        self.workspace = Workspace.objects.create(name='My Workspace', owner=self.user)
+        self.url = '/api/projects'
+
+    def test_create_project_success(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, {'name': 'My Project', 'workspace': self.workspace.id})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['name'], 'My Project')
+
+    def test_create_project_unauthenticated(self):
+        response = self.client.post(self.url, {'name': 'My Project', 'workspace': self.workspace.id})
+        self.assertEqual(response.status_code, 401)
+
+    def test_create_project_missing_name(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, {'workspace': self.workspace.id})
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_project_workspace_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, {'name': 'My Project', 'workspace': '00000000-0000-0000-0000-000000000000'})
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_project_workspace_belongs_to_other_user(self):
+        self.client.force_authenticate(user=self.other_user)
+        response = self.client.post(self.url, {'name': 'My Project', 'workspace': self.workspace.id})
+        self.assertEqual(response.status_code, 404)
+
+
+class ProjectDetailViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user('john@example.com', 'John', 'Xk9#mP2$qR7!')
+        self.other_user = User.objects.create_user('jane@example.com', 'Jane', 'Xk9#mP2$qR7!')
+        self.workspace = Workspace.objects.create(name='My Workspace', owner=self.user)
+        self.project = Project.objects.create(name='My Project', workspace=self.workspace)
+
+    def url(self, project_id):
+        return f'/api/projects/{project_id}'
+
+    def test_update_project_success(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(self.url(self.project.id), {'name': 'Renamed'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'Renamed')
+
+    def test_update_project_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(self.url('00000000-0000-0000-0000-000000000000'), {'name': 'X'})
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_project_from_other_user(self):
+        self.client.force_authenticate(user=self.other_user)
+        response = self.client.put(self.url(self.project.id), {'name': 'Hacked'})
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_project_unauthenticated(self):
+        response = self.client.put(self.url(self.project.id), {'name': 'X'})
+        self.assertEqual(response.status_code, 401)
+
+    def test_delete_project_success(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(self.url(self.project.id))
+        self.assertEqual(response.status_code, 200)
+        self.project.refresh_from_db()
+        self.assertTrue(self.project.deleted)
+        self.assertIsNotNone(self.project.deleted_at)
+
+    def test_delete_project_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(self.url('00000000-0000-0000-0000-000000000000'))
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_project_from_other_user(self):
+        self.client.force_authenticate(user=self.other_user)
+        response = self.client.delete(self.url(self.project.id))
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_project_unauthenticated(self):
+        response = self.client.delete(self.url(self.project.id))
+        self.assertEqual(response.status_code, 401)
+
+    def test_delete_already_deleted_project(self):
+        self.client.force_authenticate(user=self.user)
+        self.project.deleted = True
+        self.project.save()
+        response = self.client.delete(self.url(self.project.id))
+        self.assertEqual(response.status_code, 404)
+
+
+class ProjectsByWorkspaceViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user('john@example.com', 'John', 'Xk9#mP2$qR7!')
+        self.other_user = User.objects.create_user('jane@example.com', 'Jane', 'Xk9#mP2$qR7!')
+        self.workspace = Workspace.objects.create(name='My Workspace', owner=self.user)
+
+    def url(self, workspace_id):
+        return f'/api/workspaces/{workspace_id}/projects'
+
+    def test_list_projects_success(self):
+        self.client.force_authenticate(user=self.user)
+        Project.objects.create(name='Project A', workspace=self.workspace)
+        Project.objects.create(name='Project B', workspace=self.workspace)
+        response = self.client.get(self.url(self.workspace.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], 2)
+
+    def test_list_projects_excludes_deleted(self):
+        self.client.force_authenticate(user=self.user)
+        Project.objects.create(name='Active', workspace=self.workspace)
+        Project.objects.create(name='Deleted', workspace=self.workspace, deleted=True)
+        response = self.client.get(self.url(self.workspace.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(response.data['results'][0]['name'], 'Active')
+
+    def test_list_projects_workspace_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(self.url('00000000-0000-0000-0000-000000000000'))
+        self.assertEqual(response.status_code, 404)
+
+    def test_list_projects_workspace_belongs_to_other_user(self):
+        self.client.force_authenticate(user=self.other_user)
+        response = self.client.get(self.url(self.workspace.id))
+        self.assertEqual(response.status_code, 404)
+
+    def test_list_projects_unauthenticated(self):
+        response = self.client.get(self.url(self.workspace.id))
+        self.assertEqual(response.status_code, 401)
